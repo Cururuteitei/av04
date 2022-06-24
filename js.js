f0d91da441b3febd4c34a85b715088ecee9eeaf0
Cururuteitei <102761509+Cururuteitei@users.noreply.github.com>
2022-06-24 15:23:22 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function passarSlide(passo){
+    let atual = parseInt(document.querySelector('#atual').value ) + passo;
+    exibirSlide(atual);
+}
+imagem.src = slide.foto.replace('/home/ifce-mpe-626586/Área de Trabalho/prova av4/foto/Captura de tela de 2022-04-06 13-16-08.png') 
